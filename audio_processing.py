86f1b837761030e5a224d99a150dd6c8573d61fd
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-09-18 00:00:57 +0700

    process a exist wav file

--- audio_processing.py
+++ audio_processing.py
@@ -1,73 +1,73 @@
-# python -m pip install pyaudio
-import pyaudio
-import wave
+import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
 import numpy as np
+import librosa
 
-FRAMES_PER_BUFFER = 3200 #number of frames per buffer
-FORMAT = pyaudio.paInt16 #16 bit int
-CHANNELS = 1 #mono
-RATE = 16000 #fs = sample rate
+# Đọc file âm thanh WAV
+file_path = '/home/domanhcuong/development/python/python_audio_sample.wav'  # Đường dẫn tuyệt đối
+fs, data = wavfile.read(file_path)
 
-pa = pyaudio.PyAudio()
+# Kiểm tra số kênh của file âm thanh
+if data.ndim == 1:
+    num_channels = 1
+    print("File này có 1 kênh (Mono)")
+else:
+    num_channels = data.shape[1]
+    print(f"File này có {num_channels} kênh")
 
-stream = pa.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    frames_per_buffer=FRAMES_PER_BUFFER
-)
+# Trường hợp file WAV có nhiều kênh (ví dụ stereo)
+if num_channels > 1:
+    # Tách dữ liệu của từng kênh (kênh trái và kênh phải)
+    channel_1 = data[:, 0]  # Kênh trái
+    channel_2 = data[:, 1]  # Kênh phải
+    
+    # Tạo trục thời gian
+    num_samples = len(channel_1)
+    time = np.linspace(0, num_samples / fs, num=num_samples)
+    
+    # Vẽ tín hiệu của từng kênh
+    plt.figure(figsize=(12, 8))
+    
+    plt.subplot(2, 1, 1)
+    plt.plot(time, channel_1)
+    plt.title('Kênh trái')
+    plt.xlabel('Thời gian (giây)')
+    plt.ylabel('Biên độ')
+    
+    plt.subplot(2, 1, 2)
+    plt.plot(time, channel_2)
+    plt.title('Kênh phải')
+    plt.xlabel('Thời gian (giây)')
+    plt.ylabel('Biên độ')
 
-print('start recording')
-
-seconds = 5
-frames = []
-second_tracking = 0
-second_count = 0
-for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
-    data = stream.read(FRAMES_PER_BUFFER)
-    frames.append(data)
-    second_tracking += 1
-    if second_tracking == RATE/FRAMES_PER_BUFFER:
-        second_count += 1
-        second_tracking = 0
-        print(f'Time Left: {seconds - second_count} seconds')
-
-
-stream.stop_stream()
-stream.close()
-pa.terminate()
-
-obj = wave.open('python_audio.wav', 'wb')
-obj.setnchannels(CHANNELS)
-obj.setsampwidth(pa.get_sample_size(FORMAT))
-obj.setframerate(RATE)
-obj.writeframes(b''.join(frames))
-obj.close()
-
-
-file = wave.open('python_audio.wav', 'rb')
-
-sample_freq = file.getframerate()
-frames = file.getnframes()
-signal_wave = file.readframes(-1)
-
-file.close()
-
-time = frames / sample_freq
+    plt.tight_layout()
+    plt.show()
+else:
+    # Vẽ tín hiệu âm thanh nếu chỉ có 1 kênh
+    time = np.linspace(0, len(data) / fs, num=len(data))
+    plt.figure(figsize=(15, 7))
+    plt.plot(time, data)
+    plt.title('Tín hiệu âm thanh (Mono)')
+    plt.xlabel('Thời gian (giây)')
+    plt.ylabel('Biên độ')
+    plt.text(0.5, -0.1, f'Sample Rate (fs): {fs} Hz', ha='center', va='center', transform=plt.gca().transAxes)
+    #plt.show()
+    plt.savefig('audio_signal_sample.png')
 
 
-# if one channel use int16, if 2 use int32
-audio_array = np.frombuffer(signal_wave, dtype=np.int16)
+y, sr = librosa.load(file_path, sr=None)
+f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'))
 
-times = np.linspace(0, time, num=frames)
+# Lấy giá trị tần số cơ bản trung bình
+f0_mean = np.nanmean(f0)
 
-plt.figure(figsize=(15, 5))
-plt.plot(times, audio_array)
-plt.ylabel('Signal Wave')
-plt.xlabel('Time (s)')
-plt.xlim(0, time)
-plt.title('Do Manh Cuong recording')
-plt.savefig('sample-recorded-signal-wave.png')
-#plt.show() If you want to see the plot and make sure you have GUI installed
+# Vẽ đồ thị tần số cơ bản
+times = librosa.times_like(f0)
+plt.figure(figsize=(15, 7))
+plt.plot(times, f0, label='Tần số cơ bản (f0)', color='r')
+plt.xlabel('Thời gian (s)')
+plt.ylabel('Tần số (Hz)')
+plt.title('Tần số cơ bản của giọng nói')
+plt.legend()
+plt.text(0.5, -0.1, f'Average Fundamental Frequency (f0): {f0_mean:.2f} Hz', ha='center', va='center', transform=plt.gca().transAxes)
+plt.savefig('audio_f0_sample.png')
