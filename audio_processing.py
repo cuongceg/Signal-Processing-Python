f91ba95ea7ca9a927a527e6336baf30e14e18c75
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-09-17 11:32:43 +0700

    Every thing is done

---
+++ audio_processing.py
@@ -0,0 +1,73 @@
+# python -m pip install pyaudio
+import pyaudio
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+
+FRAMES_PER_BUFFER = 3200 #number of frames per buffer
+FORMAT = pyaudio.paInt16 #16 bit int
+CHANNELS = 1 #mono
+RATE = 16000 #fs = sample rate
+
+pa = pyaudio.PyAudio()
+
+stream = pa.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+)
+
+print('start recording')
+
+seconds = 5
+frames = []
+second_tracking = 0
+second_count = 0
+for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+    second_tracking += 1
+    if second_tracking == RATE/FRAMES_PER_BUFFER:
+        second_count += 1
+        second_tracking = 0
+        print(f'Time Left: {seconds - second_count} seconds')
+
+
+stream.stop_stream()
+stream.close()
+pa.terminate()
+
+obj = wave.open('python_audio.wav', 'wb')
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(pa.get_sample_size(FORMAT))
+obj.setframerate(RATE)
+obj.writeframes(b''.join(frames))
+obj.close()
+
+
+file = wave.open('python_audio.wav', 'rb')
+
+sample_freq = file.getframerate()
+frames = file.getnframes()
+signal_wave = file.readframes(-1)
+
+file.close()
+
+time = frames / sample_freq
+
+
+# if one channel use int16, if 2 use int32
+audio_array = np.frombuffer(signal_wave, dtype=np.int16)
+
+times = np.linspace(0, time, num=frames)
+
+plt.figure(figsize=(15, 5))
+plt.plot(times, audio_array)
+plt.ylabel('Signal Wave')
+plt.xlabel('Time (s)')
+plt.xlim(0, time)
+plt.title('Do Manh Cuong recording')
+plt.savefig('sample-recorded-signal-wave.png')
+#plt.show() If you want to see the plot and make sure you have GUI installed
